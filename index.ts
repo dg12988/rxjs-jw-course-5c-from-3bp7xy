b0dba98ca2dd6257b7680c18388871a3b21007a6
dg12988 <doug.goldberg@me.com>
2021-08-10 02:07:44 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
 import { from } from "rxjs";
 
-from(['Alice', 'Ben', 'Charlie']).subscribe({
+const somePromise = new Promise((resolve, reject) => {
+  // resolve('Resolved');
+  reject('Rejected');
+});
+
+const observableFromPromise$ = from(somePromise);
+
+observableFromPromise$.subscribe({
   next: value => console.log(value),
+  error: err => console.log('Error:', err),
   complete: () => console.log('Completed')
 });
